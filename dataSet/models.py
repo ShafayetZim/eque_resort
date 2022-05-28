6feefe104fc9280b253155c8febe660910a0132f
ShafayetZim <shafayet.zim@northsouth.edu>
2022-05-28 11:09:15 +0600

    profile and category related model and forms added

--- dataSet/models.py
+++ dataSet/models.py
@@ -1,3 +1,16 @@
+from re import I
 from django.db import models
+from django.utils import timezone
+from django.dispatch import receiver
+from more_itertools import quantify
+from django.db.models import Sum
 
-# Create your models here.
+
+# category
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
