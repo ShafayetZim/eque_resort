606826e2d3e4f51c4d47cc30302353a726ff49eb
ShafayetZim <shafayet.zim@northsouth.edu>
2022-05-29 18:45:52 +0600

    product, stock, sale, purchase, invoice model added

--- dataSet/models.py
+++ dataSet/models.py
@@ -14,3 +14,127 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# product
+class Product(models.Model):
+    code = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    brand = models.CharField(max_length=250, blank=True, null=True)
+    unit = models.CharField(max_length=250, blank=True, null=True)
+    rate = models.FloatField(default=0, blank=True, null=True)
+    price = models.FloatField(default=0)
+    status = models.CharField(max_length=2, choices=(('1', 'Active'), ('2', 'Inactive')), default=1)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + ' - ' + self.code
+
+    def count_inventory(self):
+        stocks = Stock.objects.filter(product=self)
+        stockIn = 0
+        stockOut = 0
+        for stock in stocks:
+            if stock.type == '1':
+                stockIn = int(stockIn) + int(stock.quantity)
+            else:
+                stockOut = int(stockOut) + int(stock.quantity)
+        available = stockIn - stockOut
+        return available
+
+
+# stock
+class Stock(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+    type = models.CharField(max_length=2,choices=(('1','Stock-in'),('2','Stock-Out')), default=1)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.code + ' - ' + self.product.name
+
+
+# invoice
+class Invoice(models.Model):
+    transaction = models.CharField(max_length = 250)
+    customer = models.CharField(max_length = 250)
+    total = models.FloatField(default= 0)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.transaction
+
+    def item_count(self):
+        return Invoice_Item.objects.filter(invoice = self).aggregate(Sum('quantity'))['quantity__sum']
+
+
+# invoice items
+class Invoice_Item(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, blank=True, null=True)
+    buy_price = models.FloatField(default=0)
+    price = models.FloatField(default=0)
+    quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.invoice.transaction
+
+
+@receiver(models.signals.post_save, sender=Invoice_Item)
+def stock_update(sender, instance, **kwargs):
+    stock = Stock(product=instance.product, quantity=instance.quantity, type=2)
+    stock.save()
+    # stockID = Stock.objects.last().id
+    Invoice_Item.objects.filter(id=instance.id).update(stock=stock)
+
+
+@receiver(models.signals.post_delete, sender=Invoice_Item)
+def delete_stock(sender, instance, **kwargs):
+    try:
+        stock = Stock.objects.get(id=instance.stock.id).delete()
+    except:
+        return instance.stock.id
+
+
+# purchase invoice
+class IncomingInvoice(models.Model):
+    transaction = models.CharField(max_length = 250)
+    customer = models.CharField(max_length = 250)
+    total = models.FloatField(default= 0)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.transaction
+
+    def item_count(self):
+        return IncomingInvoice_Item.objects.filter(invoice = self).aggregate(Sum('quantity'))['quantity__sum']
+
+
+# purchase invoice items
+class IncomingInvoice_Item(models.Model):
+    invoice = models.ForeignKey(IncomingInvoice, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, blank=True, null=True)
+    buy_price = models.FloatField(default=0)
+    price = models.FloatField(default=0)
+    quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.invoice.transaction
+
+
+@receiver(models.signals.post_save, sender=IncomingInvoice_Item)
+def incoming_stock_update(sender, instance, **kwargs):
+    stock = Stock(product=instance.product, quantity=instance.quantity, type=1)
+    stock.save()
+    # stockID = Stock.objects.last().id
+    IncomingInvoice_Item.objects.filter(id=instance.id).update(stock=stock)
+
+
+
+
