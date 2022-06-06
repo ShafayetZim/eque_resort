4cb6c9711e42f142e9adb6b33a1074d530d7c5d2
ShafayetZim <shafayet.zim@northsouth.edu>
2022-06-06 16:54:34 +0600

    database

--- dataSet/admin.py
+++ dataSet/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Stock)
+admin.site.register(Invoice)
+admin.site.register(Invoice_Item)
+admin.site.register(IncomingInvoice)
+admin.site.register(IncomingInvoice_Item)
